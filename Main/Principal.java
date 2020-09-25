b4f52476c6481c60a3ee3acc2045d33eb2d1724e
Pedro Trujillo García <pedrotg0312@gmail.com>
2020-09-25 17:16:08 -0500

    Create Principal.java

---
+++ Main/Principal.java
@@ -0,0 +1,56 @@
+package Main;
+
+import Logica.Operaciones;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		/*
+		 * Menú principal
+		 * 1.- Suma 
+		 * 2.- División 
+		 * 3.- Multiplicación
+		 * 4.- Resta
+		 * 5.- Salir
+		 * 
+		 * 8 --- Esa opción no existe
+		 * 
+		 * 1.- Suma
+		 * 
+		 * dame 1 numero: 77
+		 * 
+		 * 
+		 * dame otro numero: 899
+		 * 
+		 * 
+		 * El resultado es: 788
+		 * 
+		 * 
+		 * ¿Desea continuar? 1.- Si (menú principal), 2.- Terminar programa
+		 * 
+		 * 2.- Termina el programa
+		 */
+		
+		//ciclo while, o do while
+		
+		//Crear menú
+		
+		//switch......if...if else
+		
+			//case 1:
+				//intancia.resta();
+				//default;
+		
+		//termina switch
+		
+		//preguntar al cliente
+		
+		//cambia el valor de la variable del while
+		
+		//termina el while
+	}
+
+}
